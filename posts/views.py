e7f788025e54360480bbb26561def78ecf2795f2
william ricardo ariza velez <wilarizav@gmail.com>
2019-07-15 21:26:22 -0500

    feat: implement feed

--- posts/views.py
+++ posts/views.py
@@ -1,45 +1,44 @@
 """Posts views."""
 
 # Django
-from django.http import HttpResponse
 
 # Utilities
 from datetime import datetime
 from random import randint
 
+from django.shortcuts import render
+
 
 posts = [
     {
-        'name': 'All post magic',
-        'user': 'william',
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H-%M hrs'),
-        'picture': 'https://picsum.photos/id/{pid}/200/300'.format(pid=randint(1, 100))
-
+        'title': 'Mont Blanc',
+        'user': {
+            'name': 'Yésica Cortés',
+            'picture': 'https://picsum.photos/60/60/?image=1027'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/id/{pid}/400/400'.format(pid=randint(1, 100)),
     },
     {
-        'name': 'All post magic',
-        'user': 'william',
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H-%M hrs'),
-        'picture': 'https://picsum.photos/id/{pid}/200/300'.format(pid=randint(1, 100))
-
+        'title': 'Via Láctea',
+        'user': {
+            'name': 'Christian Van der Henst',
+            'picture': 'https://picsum.photos/60/60/?image=1005'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/id/{pid}/400/400'.format(pid=randint(1, 100)),
     },
     {
-        'name': 'All post magic',
-        'user': 'william',
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H-%M hrs'),
-        'picture': 'https://picsum.photos/id/{pid}/200/300'.format(pid=randint(1, 100))
-
-    },
-
+        'title': 'Nuevo auditorio',
+        'user': {
+            'name': 'Uriel (thespianartist)',
+            'picture': 'https://picsum.photos/60/60/?image=883'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/id/{pid}/400/400'.format(pid=randint(1, 100)),
+    }
 ]
 
 
 def list_posts(request):
-    content = []
-    for post in posts:
-        content.append("""
-                    <p><strong>{name}</strong></p>
-                    <p><small>{user} - <i>{timestamp}</i></small></p>
-                    <figure><img src="{picture}"/></figure>
-                """.format(**post))
-    return HttpResponse('<br>'.join(content))
+    return render(request, 'feed.html', {'posts': posts})
